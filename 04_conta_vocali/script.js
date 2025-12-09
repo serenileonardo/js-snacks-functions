820051883ce9f28a5f21c3e04dcaeeab5ea471f7
serenileonardo <serenileonardo.web@gmail.com>
2025-12-09 14:44:02 +0100

    feat: finished exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVocals(str) {
+  const vocals = "aeiou";   
+  let count = 0;            
 
+  for (let i = 0; i < str.length; i++) {
+    if (vocals.includes(str[i])) {  
+      count++;                      
+    }
+  }
+
+  return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVocals(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
