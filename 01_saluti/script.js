5000709553567cb0890bf75cdbf9c4c952c8b471
serenileonardo <serenileonardo.web@gmail.com>
2025-12-09 14:10:43 +0100

    feat: finished

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,13 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(name) {
+  return `Ciao ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
