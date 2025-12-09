f3c88bcb1c499383cc264bdd15f25baa59ba31da
serenileonardo <serenileonardo.web@gmail.com>
2025-12-09 14:38:46 +0100

    feat: exercise 3 finished

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterByLetter(arr, letter) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === letter) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(filterByLetter(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
